d93b16f823dcd65e0d90e2e9542360afc77c057c
Bernd Künnen <bernd.kuennen@gmail.com>
2026-04-27 22:16:43 +0200

    fixed assets

--- ktailor-demo/foreground.sh
+++ ktailor-demo/foreground.sh
@@ -3,15 +3,16 @@
   set +x
   set +v
   clear
-  echo "✨ Welcome to the kTailor Demo!"
-  echo "Shortcuts 'kc' and 'ns' have been added to your shell."
-  echo -n "Booting kTailor... "
+  echo -e "\n✨ Welcome to the kTailor Demo!\n"
+  echo    "Shortcuts 'kc' and 'ns' have been added to your shell."
+  echo -n "Booting kTailor..."
 
   while [ ! -f /root/.background_ready ]; do
     sleep 2
     echo -n "."
   done
 
-  echo ""
-  echo "✅ Ready! Click 'Start' on the right."
+  echo -e "\n✅ Ready! Click 'Start' on the right.\n"
+
+  bash
 } 2>/dev/null
