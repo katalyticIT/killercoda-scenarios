bcb4b40877de56a24de6ac4a71cf4c77071e7173
Bernd Künnen <bernd.kuennen@gmail.com>
2026-04-28 22:48:11 +0200

    debug setup

--- ktailor-demo/background.sh
+++ ktailor-demo/background.sh
@@ -15,11 +15,18 @@ kubectl create namespace ktailor
 # 3. Install cert-manager
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.0/cert-manager.yaml
 
-# 4. Wait for cert-manager
-kubectl wait --for=condition=Available deployment/cert-manager-webhook -n cert-manager --timeout=120s
-if [ $? -ne 0 ] ; then
+# 4. Wait for cert-manager pods
+kubectl wait --for=condition=Available deployment/cert-manager-cainjector -n cert-manager --timeout=120s
+EC=$?
+kubectl wait --for=condition=Available deployment/cert-manager-webhook    -n cert-manager --timeout=30s
+let EC=$EC+$?
+kubectl wait --for=condition=Available deployment/cert-manager            -n cert-manager --timeout=10s
+let EC=$EC+$?
+
+if [ $EC -ne 0 ] ; then
   echo >/root/error.txt "Certificate manager couldn't be installed (in time). Perhaps there's not enough free ressources. Please try again later."
 fi
+touch /root/.certmanager_ready
 
 echo "========"
 kubectl get pods -A
@@ -30,9 +37,11 @@ kubectl apply -f assets/setup.yaml
 
 # Wait for kTailor
 kubectl wait --for=condition=Available deployment/ktailor -n ktailor --timeout=60s
+touch /tmp/.ktailor_ready
 
 # 6. Install apps
 kubectl apply -f assets/apps.yaml
 kubectl wait --for=condition=Available deployment/timetravel-app -n default --timeout=60s
+touch /tmp/.apps_ready
 
 touch /root/.background_ready
--- ktailor-demo/foreground.sh
+++ ktailor-demo/foreground.sh
@@ -4,12 +4,27 @@
   set +v
   clear
   echo -e "\n✨ Welcome to the kTailor Demo! \n"
-  echo -n "Installing the kTailor webhook ..."
 
-  while [ ! -f /root/.background_ready ]; do
+  echo -n "Installing the certificate manager ..."
+  while [ ! -f /root/.certmanager_ready ]; do
     sleep 2
     echo -n "."
   done
+  echo " done."
+
+  echo -n "Installing the kTailor webhook ..."
+  while [ ! -f /root/.ktailor_ready ]; do
+    sleep 2
+    echo -n "."
+  done
+  echo " done."
+
+  echo -n "Installing the apps ..."
+  while [ ! -f /root/.apps_ready ]; do
+    sleep 2
+    echo -n "."
+  done
+  echo " done."
 
   if [ -f /root/error.txt ]; then
     echo -e "\nWe're sorry, something went wrong in the setup:"
